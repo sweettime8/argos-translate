3c2a50cf712106cd7e69d052c8e50c2b7e2de07e
nghduc91 <nghduc91@gmail.com>
2023-10-08 22:40:02 +0700

    update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import argostranslate.package
 import argostranslate.translate
+import os
 
 from_code = "en"
 to_code = "ja"
 
-#Download and install Argos Translate package
+# Download and install Argos Translate package
 argostranslate.package.update_package_index()
 available_packages = argostranslate.package.get_available_packages()
 available_package = list(
@@ -12,17 +13,21 @@ available_package = list(
         lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
     )
 )[0]
-download_path = available_package.download()
+# download_path = available_package.download()
+base_dir = os.getcwd()
+file_name = "translate-" + from_code + "_" + to_code + ".argosmodel"
+download_path = os.path.join(base_dir, "translate_data_model", file_name)
+print(download_path)
 argostranslate.package.install_from_path(download_path)
 
 # Translate
 installed_languages = argostranslate.translate.get_installed_languages()
 from_lang = list(filter(
-        lambda x: x.code == from_code,
-        installed_languages))[0]
+    lambda x: x.code == from_code,
+    installed_languages))[0]
 to_lang = list(filter(
-        lambda x: x.code == to_code,
-        installed_languages))[0]
+    lambda x: x.code == to_code,
+    installed_languages))[0]
 translation = from_lang.get_translation(to_lang)
 text = f"""
         Click on the Manage Packages menu item.
@@ -37,7 +42,4 @@ for item in available_packages:
     print(item)
 print(translatedText)
 
-
-
 # '¡Hola Mundo!'
-
